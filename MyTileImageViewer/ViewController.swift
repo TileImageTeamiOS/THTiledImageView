adcb4f873aa23b31e302530746546fe8c3fd1c17
Changnam Hong <hcn1519@gmail.com>
2018-01-08 17:39:39 +0900

    Generate TileImage 수정

--- MyTileImageViewer/ViewController.swift
+++ MyTileImageViewer/ViewController.swift
@@ -20,12 +20,13 @@ class ViewController: UIViewController {
                                CGSize(width: 512, height: 512), CGSize(width: 256, height: 256),
                                CGSize(width: 128, height: 128)]
 
-        UIImage.saveTileOf(size: tiles, name: "bench")
+        UIImage.saveTileOf(size: tiles, name: "bench", withExtension: "jpg")
+
         let imageSize = CGSize(width: 5214, height: 7300)
+        let thumbnailImageURL = Bundle.main.url(forResource: "smallBench", withExtension: "jpg")!
 
-        let imageURL = Bundle.main.url(forResource: "smallBench", withExtension: "jpg")!
+        setupExample(imageSize: imageSize, tileSize: tiles, imageURL: thumbnailImageURL)
 
-        setupExample(imageSize: imageSize, tileSize: tiles, imageURL: imageURL)
     }
 
     func setupExample(imageSize: CGSize, tileSize: [CGSize], imageURL: URL) {
@@ -40,6 +41,8 @@ class ViewController: UIViewController {
         dataSource?.minTileLevel = 1
 
         dataSource?.maxZoomLevel = 8
+
+        dataSource?.imageExtension = "jpg"
         tileImageScrollView.set(dataSource: dataSource!)
 
         dataSource?.requestBackgroundImage { _ in
@@ -58,90 +61,3 @@ class ViewController: UIViewController {
     }
 }
 
-extension UIImage {
-    class func saveTileOf(size: [CGSize], name: String) {
-
-        let levels = size.enumerated().map { (idx, _) in
-            return idx + 1
-        }
-
-        let cachesPath = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true)[0] as String
-
-        print(cachesPath)
-
-        let fileManager = FileManager.default
-        let imageNamePath = "\(cachesPath)/\(name)"
-
-        if !fileManager.fileExists(atPath: imageNamePath) {
-            do {
-                // Create Container Directory
-                try fileManager.createDirectory(atPath: imageNamePath,
-                                                withIntermediateDirectories: false, attributes: nil)
-            } catch let error as NSError {
-                print("Error creating directory: \(error.localizedDescription)")
-            }
-        } else {
-            return
-        }
-
-        size.enumerated().forEach { (idx, imageSize) in
-
-            let imageDefaultPath = "\(imageNamePath)/\(Int(imageSize.width))"
-            let filePath = "\(imageDefaultPath)/\(name)_\(Int(imageSize.width))_\(levels[idx])_0_0.png"
-            let fileExists = fileManager.fileExists(atPath: filePath)
-
-            if fileExists == false {
-                do {
-                    // Create Directory By Size
-                    try fileManager.createDirectory(atPath: imageDefaultPath,
-                                                    withIntermediateDirectories: false, attributes: nil)
-
-                } catch let error as NSError {
-                    print("Error creating directory: \(error.localizedDescription)")
-                }
-
-                var tileSize = imageSize
-                let defaultTileSize = tileSize.width
-                let scale = Float(UIScreen.main.scale)
-
-                if let image = UIImage(named: "\(name).jpg") {
-                    let imageRef = image.cgImage
-                    let totalColumns = Int(ceilf(Float(image.size.width / tileSize.width)) * scale)
-                    let totalRows = Int(ceilf(Float(image.size.height / tileSize.height)) * scale)
-                    let partialColumnWidth = Int(image.size.width.truncatingRemainder(dividingBy: tileSize.width))
-                    let partialRowHeight = Int(image.size.height.truncatingRemainder(dividingBy: tileSize.height))
-
-                    DispatchQueue.global(qos: .default).async {
-                        for y in 0..<totalRows {
-                            tileSize = imageSize
-                            for x in 0..<totalColumns {
-                                if partialRowHeight > 0 && y + 1 == totalRows {
-                                    tileSize.height = CGFloat(partialRowHeight)
-                                }
-
-                                if partialColumnWidth > 0 && x + 1 == totalColumns {
-                                    tileSize.width = CGFloat(partialColumnWidth)
-                                }
-
-                                let xOffset = CGFloat(x) * defaultTileSize
-                                let yOffset = CGFloat(y) * defaultTileSize
-                                let point = CGPoint(x: xOffset, y: yOffset)
-
-                                if let tileImageRef = imageRef?.cropping(to: CGRect(origin: point, size: tileSize)),
-                                    let imageData = UIImagePNGRepresentation(UIImage(cgImage: tileImageRef)) {
-                                    let path = "\(imageDefaultPath)/" +
-                                               "\(name)_\(Int(imageSize.width))_\(levels[idx])_\(x)_\(y).png"
-                                    try? imageData.write(to: URL(fileURLWithPath: path), options: [])
-                                }
-                            }
-                        }
-
-                        print("\(imageSize.width) image cutting finish")
-                    }
-                }
-            }
-
-        }
-    }
-
-}
---
+++ README.md
@@ -0,0 +1,7 @@
+#  StorytellingKit
+
+[![Version](https://img.shields.io/badge/pod-0.1.0-blue.svg)](http://cocoapods.org/pods)
+[![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg?style=flat)](https://github.com/younatics/YNDropDownMenu/blob/master/LICENSE)
+[![Build Status](https://travis-ci.org/younatics/YNDropDownMenu.svg?branch=master)](https://travis-ci.org/younatics/YNDropDownMenu)
+[![Platform](https://img.shields.io/cocoapods/p/YNDropDownMenu.svg?style=flat)](http://cocoapods.org/pods)
+[![Swift 4.0](https://img.shields.io/badge/Swift-4.0-%23FB613C.svg)](https://developer.apple.com/swift/)
