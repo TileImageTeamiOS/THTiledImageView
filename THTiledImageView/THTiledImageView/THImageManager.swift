85f6b21fc729d463f8ca774a4e3d65e0a1476f10
Changnam Hong <hcn1519@gmail.com>
2018-02-14 02:18:16 +0900

    Setup ImageDownload Manager & ImageCache Manager by using KingFisher

--- THTiledImageView/THTiledImageView/THImageManager.swift
+++ THTiledImageView/THTiledImageView/THImageManager.swift
@@ -1,5 +1,5 @@
 //
-//  THImageCacheManager.swift
+//  THImageManager.swift
 //  THTiledImageView
 //
 //  Created by 홍창남 on 2018. 2. 8..
@@ -12,37 +12,20 @@ import Kingfisher
 typealias THImageDownloaderCompletion = (UIImage, URL) -> Void
 typealias THImageCacheCompletion = () -> Void
 
-protocol THImageManager {
-    func downloadEachTiles(path: URL, completion: @escaping THImageDownloaderCompletion)
-    func cacheEachTiles(completion: @escaping THImageDownloaderCompletion)
-
-}
-
-protocol THImageDownloaderDelegate {
-
-}
-
 class THImageDownloadManager {
-
     static let `default` = THImageDownloadManager()
 
     func downloadEachTiles(path url: URL, completion: @escaping THImageDownloaderCompletion) {
-
         ImageDownloader.default.downloadImage(with: url, retrieveImageTask: nil,
                                               options: [], progressBlock: nil) { (image, error, url, _) in
-            if let error = error {
-                print(error)
-                return
-            }
-
-            print("Downloaded Image \(String(describing: image))")
-            guard let image = image else { return }
-            guard let url = url else { return }
-
-            completion(image, url)
+                if let error = error {
+                    print(error)
+                    return
+                }
+                guard let image = image, let url = url else { return }
+                completion(image, url)
         }
     }
-
 }
 
 class THImageCacheManager {
@@ -55,12 +38,8 @@ class THImageCacheManager {
         }
     }
 
-//    func retrieveTiles(key: String, completion: @escaping THImageCacheCompletion) {
-//        ImageCache.default.retrieveImage(forKey: key, options: nil) {  image, _ in
-//            if let image = image {
-//                completion(image)
-//            }
-//        }
-//    }
+    func retrieveTiles(key: String) -> UIImage? {
+        return ImageCache.default.retrieveImageInDiskCache(forKey: key)
+    }
 
 }
